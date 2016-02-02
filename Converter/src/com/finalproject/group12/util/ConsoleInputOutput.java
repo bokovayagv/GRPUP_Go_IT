f475ef69f7d197f4e3e009c3274d8dd0853d0eed
Oleksandr Stefanovskyi <astefanovskiy@gmail.com>
2016-02-02 22:10:25 +0200

    repair class diagram

---
+++ Converter/src/com/finalproject/group12/util/ConsoleInputOutput.java
@@ -0,0 +1,13 @@
+package com.finalproject.group12.util;
+
+public class ConsoleInputOutput implements IInputOutput {
+    @Override
+    public void read() {
+
+    }
+
+    @Override
+    public void write(String arg) {
+        System.out.println(arg);
+    }
+}
